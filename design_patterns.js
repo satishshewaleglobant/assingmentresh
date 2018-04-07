7516b67183643e3384e94d588ec068b6d5f168b2
Satish Shewale <satish.shewale@emc.com>
2018-04-07 12:27:50 +0530

    Initial commit

---
+++ design_patterns.js
@@ -0,0 +1,23 @@
+
+//Module Design pattern
+
+var currentModule = (function(){
+    var name = "satish";
+    console.log(name);
+
+    // var obj = {};
+    // obj.username  =  name;
+    // obj.sum = function(){
+        
+    // }
+    return {
+        "username" : name,
+        sum : function(){
+            console.log("In Sum",name);
+        }
+    };
+
+})();
+
+currentModule.sum();
+console.log(currentModule.username);
